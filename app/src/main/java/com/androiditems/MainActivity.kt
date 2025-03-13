d1877717b836c3025d770e7472d09a712d82b66c
Jun <leej1025y@live.com>
2025-03-13 22:47:55 +1100

    feat: add viewmodel dependency

--- app/src/main/java/com/androiditems/MainActivity.kt
+++ app/src/main/java/com/androiditems/MainActivity.kt
@@ -3,7 +3,10 @@ package com.androiditems
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.androiditems.repositories.ItemsRepository
 import com.androiditems.ui.MainContent
 import com.androiditems.viewmodels.ItemViewModel
 import com.androiditems.viewmodels.ListViewModel
@@ -11,12 +14,26 @@ import com.androiditems.viewmodels.ListViewModel
 class MainActivity : ComponentActivity() {
 
     // TODO: Use dependency injection
-    private val listViewModel by viewModels<ListViewModel>()
-    private val itemViewModel by viewModels<ItemViewModel>()
+    private val itemsRepository = ItemsRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val listViewModel = viewModel<ListViewModel>(
+                factory = object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                        return ListViewModel(itemsRepository) as T
+                    }
+                }
+            )
+            val itemViewModel = viewModel<ItemViewModel>(
+                factory = object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                        return ItemViewModel(itemsRepository) as T
+                    }
+                }
+            )
+
             MainContent(listViewModel, itemViewModel)
         }
     }
