6cc8e19585f03b318193ae83ef4b11b5b9429b43
Jun <leej1025y@live.com>
2025-03-14 00:50:06 +1100

    feat: update UI if user modifies content on other platform

--- app/src/main/java/com/androiditems/repositories/ItemsRepository.kt
+++ app/src/main/java/com/androiditems/repositories/ItemsRepository.kt
@@ -3,22 +3,49 @@ package com.androiditems.repositories
 import com.androiditems.models.Item
 import com.androiditems.models.Result
 import com.androiditems.services.IItemsService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 interface IItemsRepository {
-    suspend fun loadItems(): Result<List<Item>>
+    val items: StateFlow<List<Item>>
+    suspend fun loadItems()
     suspend fun createItem(item: Item): Result<Item>
     suspend fun updateItem(item: Item): Result<Item>
     suspend fun deleteItem(item: Item): Result<String>
 }
 
 class ItemsRepository(
-//    private val cache: ICache, // TODO: Add caching
     private val itemsService: IItemsService
 ): IItemsRepository {
 
-    override suspend fun loadItems(): Result<List<Item>> {
-        // TODO: If not in cache, then fetch from API
-        return Result.Loading
+    private val _items = MutableStateFlow<List<Item>>(mutableListOf())
+    override val items = _items.asStateFlow()
+
+    override suspend fun loadItems() {
+        // TODO: add snapshot listener to get the latest items
+//        try {
+//            Firebase
+//                .firestore(FirebaseApp.initializeApp(context, options, envKey))
+//                .collection()
+//                .addSnapshotListener f@{ snapshot, error ->
+//                    if (snapshot == null) {
+//                        _items.value = Result.Loading
+//                        return@f
+//                    }
+//                    val newItems = snapshot.documents.map g@{
+//                        val jsonString = it.data.toJsonString()
+//                        val newItem = Item.decodeFromJsonString(
+//                            data = jsonString,
+//                            jsonConfig = JsonConfig(ignoreUnknownFieldsInInput = true)
+//                        )
+//                        return@g newItem
+//                    }
+//                    _items.value = Result.Success(newItems)
+//                }
+//        } catch (e: Exception) {
+//            _items.value = Result.Error(e)
+//        }
     }
 
     override suspend fun createItem(item: Item): Result<Item> {
--- app/src/main/java/com/androiditems/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/androiditems/viewmodels/ListViewModel.kt
@@ -3,39 +3,23 @@ package com.androiditems.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.androiditems.models.Item
-import com.androiditems.models.Result
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import com.androiditems.repositories.IItemsRepository
 import kotlinx.coroutines.launch
 
 interface IListViewModel {
     val items: StateFlow<List<Item>>
-//    fun refreshItems()
 }
 
 class ListViewModel(
     private val itemsRepository: IItemsRepository
 ) : ViewModel(), IListViewModel {
 
-    private val _items = MutableStateFlow<List<Item>>(mutableListOf())
-    override val items = _items.asStateFlow()
+    override val items = itemsRepository.items
 
     init {
         viewModelScope.launch f@{
-            val result = itemsRepository.loadItems()
-            when(result) {
-                is Result.Success -> {
-                    _items.value = result.data
-                }
-                is Result.Error<*> -> {
-                    // TODO: show error message
-                }
-                is Result.Loading -> {
-                    // TODO: Show CircularProgressBar
-                }
-            }
+            itemsRepository.loadItems()
         }
     }
 
