28d220a9bf48145790c3d379dacf061a97393de0
Jun <leej1025y@live.com>
2025-03-13 22:21:27 +1100

    feat: add Result

--- app/src/main/java/com/androiditems/models/models.kt
+++ app/src/main/java/com/androiditems/models/models.kt
@@ -1,8 +1,17 @@
 package com.androiditems.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Item(val id: String, val name: String) {
+    // Not required, id.equals() already exists
+//    fun equals(other: Item): Boolean =
+//        this.id == other.id && this.name == other.name
 
-    fun equals(other: Item): Boolean =
-        this.id == other.id && this.name == other.name
+}
 
+sealed class Result<out T> {
+    object Loading : Result<Nothing>()
+    data class Success<T>(val data: T): Result<T>()
+    data class Error<T>(val exception: Throwable) : Result<Nothing>()
 }
