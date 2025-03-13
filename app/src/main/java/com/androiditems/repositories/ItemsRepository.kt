5015027117902fc24fe51b3f42281745d25fc307
Jun <leej1025y@live.com>
2025-03-13 22:20:59 +1100

    feat: add ItemsRepository stub

---
+++ app/src/main/java/com/androiditems/repositories/ItemsRepository.kt
@@ -0,0 +1,16 @@
+package com.androiditems.repositories
+
+interface IItemsRepository {
+    fun loadItems()
+}
+
+class ItemsRepository(
+//    private val cache: ICache,
+//    private val itemsService: IItemsService
+): IItemsRepository {
+
+    override fun loadItems() {
+
+    }
+
+}
